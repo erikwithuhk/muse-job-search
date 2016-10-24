2ace520ce4244a84ee6fd1b709e0c71071454314
Erik Jönsson <efjonsson@gmail.com>
2016-10-24 02:53:34 -0400

    add port back in

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
