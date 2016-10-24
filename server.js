3d938c957bdbe48e8672250c4ecda97b5c214e7d
Erik Jönsson <efjonsson@gmail.com>
2016-10-24 02:53:09 -0400

    remove default port

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
